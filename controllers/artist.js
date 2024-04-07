0a1af8febb91d237c4cd0af6468c050e0af2bfd9
Julai Dangol <99350702+julaij@users.noreply.github.com>
2024-04-06 19:39:04 -0500

    complete create and update pug views

--- controllers/artist.js
+++ controllers/artist.js
@@ -24,8 +24,6 @@ exports.artist_view_all_Page = async function (req, res) {
     }
 };
 
-
-//view controllers
 // Handle a show one view with id specified by query
 exports.artist_view_one_Page = async function (req, res) {
     console.log("single view for id " + req.query.id)
@@ -33,6 +31,34 @@ exports.artist_view_one_Page = async function (req, res) {
         result = await Artist.findById(req.query.id)
         res.render('artistdetail',
             { title: 'Artist Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.artist_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('artistcreate', { title: 'Artist Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.artist_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Artist.findById(req.query.id)
+        res.render('artistupdate', { title: 'Artist Update', toShow: result });
     }
     catch (err) {
         res.status(500)
--- routes/artists.js
+++ routes/artists.js
@@ -5,6 +5,12 @@ var router = express.Router();
 /* GET detail artist page */
 router.get('/detail', artist_controlers.artist_view_one_Page);
 
+/* GET create artist page */
+router.get('/create', artist_controlers.artist_create_Page);
+
+/* GET create update page */
+router.get('/update', artist_controlers.artist_update_Page);
+
 /* GET artists */
 router.get('/', artist_controlers.artist_view_all_Page );
 
