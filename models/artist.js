762856e8578ae69e3856403c87947b172f88675c
Julai Dangol <99350702+julaij@users.noreply.github.com>
2024-04-14 13:22:54 -0500

    validate model cost field

--- models/artist.js
+++ models/artist.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const artistSchema = mongoose.Schema({
     artist_name : String, 
-    artist_masterpiece_cost: Number,
-    artist_masterpiece_name : String
+    artist_masterpiece_cost: {
+        type: Number,
+        required: true,
+        min: [0],
+        max: [10000000000]
+    },    artist_masterpiece_name : String
 })
 
 module.exports = mongoose.model("Artist", artistSchema)
