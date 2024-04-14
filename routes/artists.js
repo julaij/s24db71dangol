a21601e0cd574ae65623a393bbfe1cd67e330060
Julai Dangol <99350702+julaij@users.noreply.github.com>
2024-04-14 12:22:49 -0500

    securing endpoints

--- routes/artists.js
+++ routes/artists.js
@@ -1,6 +1,15 @@
 var express = require('express');
-const artist_controlers= require('../controllers/artist');
+const artist_controlers = require('../controllers/artist');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 /* GET detail artist page */
 router.get('/detail', artist_controlers.artist_view_one_Page);
@@ -9,13 +18,13 @@ router.get('/detail', artist_controlers.artist_view_one_Page);
 router.get('/create', artist_controlers.artist_create_Page);
 
 /* GET create update page */
-router.get('/update', artist_controlers.artist_update_Page);
+router.get('/update', secured, artist_controlers.artist_update_Page);
 
 /* GET delete artist page */
 router.get('/delete', artist_controlers.artist_delete_Page);
 
 /* GET artists */
-router.get('/', artist_controlers.artist_view_all_Page );
+router.get('/', artist_controlers.artist_view_all_Page);
 
 // GET request for one artist.
 router.get('/artists/:id', artist_controlers.artist_detail);
